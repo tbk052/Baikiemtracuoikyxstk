12124a975b91907269e2f2ac45db060cd0e7e626
Phạm Đình Thanh Bình <75012620+tbk052@users.noreply.github.com>
2022-01-14 10:43:27 +0700

    Update baikiemtra2.py

--- baikiemtra2.py
+++ baikiemtra2.py
@@ -1,14 +1,16 @@
-# Nhập tên đệm: (Thanh)
+    # Nhập tên đệm: (Thanh)
 subName = input('Nhập tên đệm (Ex: thanh):')
 print(subName)
 
 
-# Nhập tổng số kí tự của tên đệm và tên
-print("Nhập số lượng kí tự của tên đệm và tên của bạn (ex: Thanh Bình = 9):",end='')
-n=input()
+# Nhập tên đệm và tên
+print
+subName=str(input("Nhập tên đệm (ex: thanh): "));
+name=str(input("Nhập tên (ex:binh): "));
+n=str(len(subName))+str(len(name));
 n_len=len(n)
-tong=0
+sum=0
 for i in range(n_len):
     n_val=n[i]
-    tong=tong+int(n_val)
-print("Tổng các ký tự của tên đệm và tên vừa nhập là: ",tong)
+    sum=sum+int(n_val)
+print("Tổng các ký tự của tên đệm và tên vừa nhập là: ",sum)
