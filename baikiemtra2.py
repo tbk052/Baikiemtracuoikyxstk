52b5547e9d77764b5b80340a692643dd26229a7c
Phạm Đình Thanh Bình <75012620+tbk052@users.noreply.github.com>
2022-01-14 09:07:06 +0700

    Update baikiemtra2.py

--- baikiemtra2.py
+++ baikiemtra2.py
@@ -4,7 +4,7 @@ print(subName)
 
 
 # Nhập tổng số kí tự của tên đệm và tên
-print("Nhập số lượng kí tự của tên đệm và tên của bạn (ex: Thanh Bình):",end='')
+print("Nhập số lượng kí tự của tên đệm và tên của bạn (ex: Thanh Bình = 9):",end='')
 n=input()
 n_len=len(n)
 tong=0
