3cdddc970e7ce4b3a4b644364daf76e7cb6ca048
Phạm Đình Thanh Bình <75012620+tbk052@users.noreply.github.com>
2022-01-14 09:03:28 +0700

    Add files via upload

---
+++ baikiemtra2.py
@@ -0,0 +1,14 @@
+# Nhập tên đệm: (Thanh)
+subName = input('Nhập tên đệm (Ex: thanh):')
+print(subName)
+
+
+# Nhập tổng số kí tự của tên đệm và tên
+print("Nhập số lượng kí tự của tên đệm và tên của bạn (ex: Thanh Bình):",end='')
+n=input()
+n_len=len(n)
+tong=0
+for i in range(n_len):
+    n_val=n[i]
+    tong=tong+int(n_val)
+print("Tổng các ký tự của tên đệm và tên vừa nhập là: ",tong)
