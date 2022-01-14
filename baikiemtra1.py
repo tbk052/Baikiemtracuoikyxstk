019c2aae18563a92bfac5f22b25860d572cb8bb3
Phạm Đình Thanh Bình <75012620+tbk052@users.noreply.github.com>
2022-01-14 08:49:57 +0700

    Update baikiemtra1.py

--- baikiemtra1.py
+++ baikiemtra1.py
@@ -3,7 +3,10 @@ name = input('Nhập tên (Ex: binh):')
 print(name)
 
 
-for i in range((len(name))):
+# Nhập n là số lượng kí tự của tên (binh = 4)
+n = input('Nhập độ dài tên của mình:')
+n = int(n)
+for i in range(n):
     myDict = {i+1:(i+1)*(i+1)}
     print(myDict)
 
